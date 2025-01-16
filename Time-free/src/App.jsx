c0cbb27c47f09b1ccccab5361c6f94892e58d8b4
olumide933 <falowoolumide@gmail.com>
2025-01-15 21:23:14 -0800

    my first react commit

---
+++ Time-free/src/App.jsx
@@ -0,0 +1,13 @@
+import Olumy from "./assets/components/Olumy";
+
+const App = () => {
+  return(
+    <div>
+    <Olumy />
+      <p>This is the application Page</p>
+    </div>
+  )
+};
+
+
+export default App
---
+++ Time-free/src/assets/components/Olumy.jsx
@@ -0,0 +1,25 @@
+import "./Olumy.css"
+
+const Olumy =() =>{
+const myStyle ={
+  backgroundColor:"green",
+  color:"white",
+  paddind:"10px",
+};
+
+    return(
+      <div>
+        <img src="myCam.jpg" alt=""/>
+        <h1>Falowo Olumide Babajide</h1>
+        <p style={myStyle}>This is the day that the <b>lord</b> has made and i will rejoice and be gad in him.
+          Join me to praise the name of the Lord.
+        </p><button>Click to know me</button>
+
+<input type="file" />
+<select >Canada</select>
+<select name="country" id="">Canada</select>
+      </div>
+    )
+  };
+  
+  export default Olumy
