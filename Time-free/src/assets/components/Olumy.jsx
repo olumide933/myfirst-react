265bff806529c8416f813af01d498c30f9366b5d
olumide933 <falowoolumide@gmail.com>
2025-01-23 11:08:16 -0800

    addition of counter button

--- Time-free/src/assets/components/Olumy.jsx
+++ Time-free/src/assets/components/Olumy.jsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import "./Olumy.css"
 
+
 const Olumy =() =>{
+
+  const [myCounter, setMyCounter] = useState(2);
+ const increaseCount =() => {
+  setMyCounter(myCounter +3)
+};
+const DecreaseCount =() => {
+  setMyCounter(myCounter -2)
+}
 const myStyle ={
   backgroundColor:"green",
   color:"white",
   paddind:"10px",
-};
+}
 
     return(
       <div>
         <img src="myCam.jpg" alt=""/>
-        <h1>Falowo Olumide Babajide</h1>
+        <h1>Falowo Olumide Babajide {myCounter}</h1>
         <p style={myStyle}>This is the day that the <b>lord</b> has made and i will rejoice and be gad in him.
           Join me to praise the name of the Lord.
-        </p><button>Click to know me</button>
+        </p>
+        <button onClick={increaseCount}>increase</button>
+        <br />
+        <button onClick={DecreaseCount}>Decrease</button>
 
-<input type="file" />
-<select >Canada</select>
-<select name="country" id="">Canada</select>
+
       </div>
     )
   };
